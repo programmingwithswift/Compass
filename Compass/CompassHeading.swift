5ff18aae4415c8122ffc21c1098a60312f252cf4
ProgrammingWithSwift <programmingwithswift@gmail.com>
2019-10-09 15:48:52 +0200

    Initial commit

---
+++ Compass/CompassHeading.swift
@@ -0,0 +1,44 @@
+//
+//  Compass.swift
+//  Compass
+//
+//  Created by ProgrammingWithSwift on 2019/10/06.
+//  Copyright © 2019 ProgrammingWithSwift. All rights reserved.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+import CoreMotion
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.magneticHeading
+    }
+}
---
+++ Compass/ContentView.swift
@@ -0,0 +1,96 @@
+import Foundation
+import SwiftUI
+
+struct Marker: Hashable {
+    let degrees: Double
+    let label: String
+
+    init(degrees: Double, label: String = "") {
+        self.degrees = degrees
+        self.label = label
+    }
+
+    func degreeText() -> String {
+        return String(format: "%.0f", self.degrees)
+    }
+
+    static func markers() -> [Marker] {
+        return [
+            Marker(degrees: 0, label: "S"),
+            Marker(degrees: 30),
+            Marker(degrees: 60),
+            Marker(degrees: 90, label: "W"),
+            Marker(degrees: 120),
+            Marker(degrees: 150),
+            Marker(degrees: 180, label: "N"),
+            Marker(degrees: 210),
+            Marker(degrees: 240),
+            Marker(degrees: 270, label: "E"),
+            Marker(degrees: 300),
+            Marker(degrees: 330)
+        ]
+    }
+}
+
+struct CompassMarkerView: View {
+    let marker: Marker
+    let compassDegress: Double
+
+    var body: some View {
+        VStack {
+            Text(marker.degreeText())
+                .fontWeight(.light)
+                .rotationEffect(self.textAngle())
+            
+            Capsule()
+                .frame(width: self.capsuleWidth(),
+                       height: self.capsuleHeight())
+                .foregroundColor(self.capsuleColor())
+                .padding(.bottom, 120)
+            
+            Text(marker.label)
+                .fontWeight(.bold)
+                .rotationEffect(self.textAngle())
+                .padding(.bottom, 80)
+        }.rotationEffect(Angle(degrees: marker.degrees))
+    }
+    
+    private func capsuleWidth() -> CGFloat {
+        return self.marker.degrees == 0 ? 7 : 3
+    }
+
+    private func capsuleHeight() -> CGFloat {
+        return self.marker.degrees == 0 ? 45 : 30
+    }
+
+    private func capsuleColor() -> Color {
+        return self.marker.degrees == 0 ? .red : .gray
+    }
+
+    private func textAngle() -> Angle {
+        return Angle(degrees: -self.compassDegress - self.marker.degrees)
+    }
+}
+
+struct ContentView : View {
+    @ObservedObject var compassHeading = CompassHeading()
+
+    var body: some View {
+        VStack {
+            Capsule()
+                .frame(width: 5,
+                       height: 50)
+
+            ZStack {
+                ForEach(Marker.markers(), id: \.self) { marker in
+                    CompassMarkerView(marker: marker,
+                                      compassDegress: self.compassHeading.degrees)
+                }
+            }
+            .frame(width: 300,
+                   height: 300)
+            .rotationEffect(Angle(degrees: self.compassHeading.degrees))
+            .statusBar(hidden: true)
+        }
+    }
+}
